c2a55e5e8c14225362268cbe4320aadf97c1707e
Olubusayo Amowe <amoweolubusayo@gmail.com>
2021-12-13 21:45:14 +0100

    code clean up

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-web3 = new Web3(new Web3.providers.HttpProvider("https://andromeda.metis.io/?owner=1088"))
-var account;
-web3.eth.getAccounts().then((f) => {
-    account = f[0];
-})
+async function connect() {
+    const priceElem = document.querySelector('#price')
 
-abi = JSON.parse('[{"type":"constructor","stateMutability":"nonpayable","inputs":[]},{"type":"event","name":"OracleUpdate","inputs":[{"type":"string","name":"key","internalType":"string","indexed":false},{"type":"uint128","name":"value","internalType":"uint128","indexed":false},{"type":"uint128","name":"timestamp","internalType":"uint128","indexed":false}],"anonymous":false},{"type":"event","name":"UpdaterAddressChange","inputs":[{"type":"address","name":"newUpdater","internalType":"address","indexed":false}],"anonymous":false},{"type":"function","stateMutability":"view","outputs":[{"type":"uint128","name":"","internalType":"uint128"},{"type":"uint128","name":"","internalType":"uint128"}],"name":"getValue","inputs":[{"type":"string","name":"key","internalType":"string"}]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"setValue","inputs":[{"type":"string","name":"key","internalType":"string"},{"type":"uint128","name":"value","internalType":"uint128"},{"type":"uint128","name":"timestamp","internalType":"uint128"}]},{"type":"function","stateMutability":"nonpayable","outputs":[],"name":"updateOracleUpdaterAddress","inputs":[{"type":"address","name":"newOracleUpdaterAddress","internalType":"address"}]},{"type":"function","stateMutability":"view","outputs":[{"type":"uint256","name":"","internalType":"uint256"}],"name":"values","inputs":[{"type":"string","name":"","internalType":"string"}]}]')
+    try {
+        priceElem.textContent = 'Updating price...';
 
-contract = new web3.eth.Contract(abi);
-contract.options.address = "0x6E6E633320Ca9f2c8a8722c5f4a993D9a093462E";
+        const ABI = [{ "type": "constructor", "stateMutability": "nonpayable", "inputs": [] }, { "type": "event", "name": "OracleUpdate", "inputs": [{ "type": "string", "name": "key", "internalType": "string", "indexed": false }, { "type": "uint128", "name": "value", "internalType": "uint128", "indexed": false }, { "type": "uint128", "name": "timestamp", "internalType": "uint128", "indexed": false }], "anonymous": false }, { "type": "event", "name": "UpdaterAddressChange", "inputs": [{ "type": "address", "name": "newUpdater", "internalType": "address", "indexed": false }], "anonymous": false }, { "type": "function", "stateMutability": "view", "outputs": [{ "type": "uint128", "name": "", "internalType": "uint128" }, { "type": "uint128", "name": "", "internalType": "uint128" }], "name": "getValue", "inputs": [{ "type": "string", "name": "key", "internalType": "string" }] }, { "type": "function", "stateMutability": "nonpayable", "outputs": [], "name": "setValue", "inputs": [{ "type": "string", "name": "key", "internalType": "string" }, { "type": "uint128", "name": "value", "internalType": "uint128" }, { "type": "uint128", "name": "timestamp", "internalType": "uint128" }] }, { "type": "function", "stateMutability": "nonpayable", "outputs": [], "name": "updateOracleUpdaterAddress", "inputs": [{ "type": "address", "name": "newOracleUpdaterAddress", "internalType": "address" }] }, { "type": "function", "stateMutability": "view", "outputs": [{ "type": "uint256", "name": "", "internalType": "uint256" }], "name": "values", "inputs": [{ "type": "string", "name": "", "internalType": "string" }] }]
 
-$(document).ready(function() {
-    let x = contract.methods.getValue('ETH/USD').call().then(function(result) {
-        $('#getValue').html(Math.round(result[0] / 100000000))
-    });
-});
+        const web3 = new Web3(new Web3.providers.HttpProvider("https://andromeda.metis.io/?owner=1088"))
+
+        const contract = new web3.eth.Contract(ABI);
+        contract.options.address = "0x6E6E633320Ca9f2c8a8722c5f4a993D9a093462E";
+
+        const currentPriceResponse = await contract.methods.getValue('ETH/USD').call()
+
+        if (currentPriceResponse[0]) {
+            const formatter = new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
+                minimumFractionDigits: 2,
+            });
+            priceElem.textContent = formatter.format(currentPriceResponse[0] / 100000000);
+        }
+
+    } catch (e) {
+        priceElem.textContent = 'An error occurred while updating the price, please refresh your page.';
+    }
+}
+
+window.onload = function() {
+    connect();
+}
